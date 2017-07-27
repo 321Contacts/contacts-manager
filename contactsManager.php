dcbe9b0daaf42d971db262f853f67969fe0bff98
Hunter Warren <hunter.warren@gmail.com>
2017-07-27 10:44:10 -0500

    building menu fgets inputs. add contacts echos a message!

--- contactsManager.php
+++ contactsManager.php
@@ -7,21 +7,39 @@ function startSession($fileName) {
   //retrieve info on session_start, open method a+ (read and write. pointer at the end of the file, create if not existent)
   $handle = fopen($fileName, 'a+');
   $contacts = fread($handle, filesize($fileName));
-  return $contacts;
+  viewContacts($contacts);
+  menuOptions();
 }
- $list = startSession("contacts.txt");
- echo $list;  
+
 
 function menuOptions() {
   //print menu of options
+  fwrite(STDOUT, "Please enter a number to select an option.".PHP_EOL);
+  fwrite(STDOUT, "1) View Contacts".PHP_EOL);
+  fwrite(STDOUT, "2) Add Contact".PHP_EOL);
+  fwrite(STDOUT, "3) Add Contact".PHP_EOL);
+  $input = fgets(STDIN);
+  
+  if ($input == 2) {
+    addContacts();
+  }
+  if ($input == 2) {
+    addContacts();
+  }
+  if ($input == 2) {
+    addContacts();
+  }
+
 }
 
-function showList() {
+function viewContacts($contacts) {
   //print a list
+  fwrite(STDOUT, $contacts) ;
 }
 
 function addContacts() {
   //add a contacts
+  echo "Contact added :)";
 }
 
 function searchName() {
@@ -31,3 +49,4 @@ function searchName() {
 function deleteContact() {
   //delete contact
 }
+startSession("contacts.txt");

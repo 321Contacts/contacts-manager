4f4108c5680c774814ce63a460d1b81edfb4b3da
Hunter Warren <hunter.warren@gmail.com>
2017-07-27 10:19:01 -0500

    successfully opening the file!

--- contactsManager.php
+++ contactsManager.php
@@ -1,11 +1,16 @@
 <?php
 
-//we will make 5 distinct functions for each opiton in the menu
+//we will make 5 distinct functions for each option in the menu
 
 
-function startSession() {
+function startSession($fileName) {
   //retrieve info on session_start, open method a+ (read and write. pointer at the end of the file, create if not existent)
+  $handle = fopen($fileName, 'a+');
+  $contacts = fread($handle, filesize($fileName));
+  return $contacts;
 }
+ $list = startSession("contacts.txt");
+ echo $list;  
 
 function menuOptions() {
   //print menu of options

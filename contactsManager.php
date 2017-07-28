4165e9bf1190bcd536eaa23e640319f877a7ee58
Vivian Canales <viviancans@gmail.com>
2017-07-28 15:18:10 -0500

    App is working. Commit before refactor.

--- contactsManager.php
+++ contactsManager.php
@@ -17,6 +17,7 @@ viewContacts();
 
 function menuOptions()
 {
+
   fwrite(STDOUT,
     PHP_EOL .
     "1) View Contacts" . PHP_EOL .
@@ -66,46 +67,48 @@ function viewContacts()
   $fileName = "contacts.txt";
   $handle = fopen($fileName, 'r');
   $contacts = fread($handle, filesize($fileName));
+  fclose($handle);
   $contacts = trim($contacts);
+  var_dump($contacts);
 
   //table heading
-  $line1 = str_pad("CONTACTS", 50, "-", STR_PAD_BOTH);
+  $line1 = "\e[31m" . str_pad("CONTACTS", 50, "-", STR_PAD_BOTH);
   $line2 = str_pad("NAME", 24, " ", STR_PAD_RIGHT) . "|" . str_pad("NUMBER", 25, " ", STR_PAD_LEFT);
-  $line3 = str_pad("", 50, "-", STR_PAD_BOTH);
-  $heading = $line1 . PHP_EOL . PHP_EOL . $line2 . PHP_EOL . $line3 . PHP_EOL;
+  $line3 = str_pad("", 50, "-", STR_PAD_BOTH) . "\e[0m";
+  $heading =  $line1 . PHP_EOL . PHP_EOL . $line2 . PHP_EOL . $line3 . PHP_EOL;
 
   //explode data for table formatting
   $formatArray = explode("\n", $contacts);
-  $arrayArrays = [];
-  foreach($formatArray as $key => $value) {
-    $arrayArrays[$key] = explode("|", $value);
-    $arrayArrays[$key][1] = substr($arrayArrays[$key][1], 0, 3) . "-" . substr($arrayArrays[$key][1], 3, 3) . "-" . substr($arrayArrays[$key][1], 6);
-  }
+  // $arrayArrays = [];
 
+
+  // foreach($formatArray as $key => $value) {
+  //   $arrayArrays[$key] = explode("|", $value);
+  //   $arrayArrays[$key][1] = substr($arrayArrays[$key][1], 0, 3) . "-" . substr($arrayArrays[$key][1], 3, 3) . "-" . substr($arrayArrays[$key][1], 6);
+  // }
+
+  // print_r($formatArray);
 
   //echo to print table
-  echo $heading;
-  foreach($arrayArrays as $key => $value) {
-    echo str_pad($value[0], 24, " ", STR_PAD_RIGHT);
-    echo "|";
-    echo str_pad($value[1], 25, " ", STR_PAD_LEFT) . PHP_EOL;
-  }
+  // echo $heading;
+  // foreach($arrayArrays as $key => $value) {
+  //   echo str_pad($value[0], 24, " ", STR_PAD_RIGHT);
+  //   echo "|";
+  //   echo str_pad($value[1], 25, " ", STR_PAD_LEFT) . PHP_EOL;
+  // }
 
-
+  // print_r($arrayArrays);
+ 
   menuOptions();
 }
-
-
 
 
 function addContact()
 {
   system('clear');
 
-
   $fileName = "contacts.txt";
   $handle = fopen($fileName, 'a+');
-  $contacts = fread($handle, filesize($fileName));
 
   fwrite(STDOUT, "Please enter first name:  ");
   $firstName = trim(fgets(STDIN));
@@ -122,7 +125,6 @@ function addContact()
 
   viewContacts();
   menuOptions();
-
 }
 
 
@@ -150,7 +152,6 @@ function searchName()
   }
   fclose($handle);
 
-  viewContacts();
   menuOptions();
 }
 
